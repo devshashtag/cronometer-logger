22cb88c89f17a6a38e2ecd86dd0ff3642d97640f
devs.hashtag <devs.hashtag@gmail.com>
2025-04-14 20:21:11 +0330

    config upload and download added

--- assets/js/modules/interface.js
+++ assets/js/modules/interface.js
@@ -118,6 +118,20 @@ class Interface {
     });
   }
 
+  setConfigEvents() {
+    const download = document.getElementById('download');
+    const upload = document.getElementById('upload');
+
+    download.addEventListener('click', () => {
+      storage.downloadConfig();
+    });
+
+    upload.addEventListener('click', async () => {
+      await storage.uploadConfig();
+      window.location.reload();
+    });
+  }
+
   updateOnRunning() {
     if (storage.isRunning()) {
       this.interval = setInterval(() => {
@@ -130,12 +144,11 @@ class Interface {
     }
   }
 
-  removeRecord() {}
-
   // initialize ui
   init() {
     this.generateHistoryRecords();
     this.setHistoryEvents();
+    this.setConfigEvents();
     this.updateDateTime();
     this.updateDurations();
     this.updateOnRunning();
